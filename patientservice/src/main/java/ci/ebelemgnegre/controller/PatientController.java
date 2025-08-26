3a0fd73af1c86b5274a3795460761c05f42cc174
Etienne BELEMGNEGRE <ebelemgnegre@influo.ci>
2025-08-26 09:36:24 +0000

    Refactor Docker configuration for patient service by removing production files and updating development settings to use H2 in-memory database. Adjusted environment variables in docker-compose and application properties for improved local testing.

--- patientservice/src/main/java/ci/ebelemgnegre/controller/PatientController.java
+++ patientservice/src/main/java/ci/ebelemgnegre/controller/PatientController.java
@@ -1,10 +1,28 @@
 package ci.ebelemgnegre.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ci.ebelemgnegre.dto.PatientResponseDTO;
+import ci.ebelemgnegre.service.PatientService;
 
 @RestController
 @RequestMapping("/patients")
 public class PatientController {
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients(){
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
+    }
     
 }
